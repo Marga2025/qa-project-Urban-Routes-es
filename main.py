404a5197159e30534bb55e443a2d0dbe45921b9e
“MARGARITA <MARITA0585@HOTMAIL.COM>
2025-08-13 08:47:22 -0500

    se actualizo codigo en el diligenciamiento de la prueba de direccion

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pytest
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
-from urban_routes_utils import retrieve_phone_code
+
 
 
 import data
