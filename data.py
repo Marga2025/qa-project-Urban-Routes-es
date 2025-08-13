62b03f044c4cb25411493788cba4a6d012010ed8
“MARGARITA <MARITA0585@HOTMAIL.COM>
2025-08-13 08:46:23 -0500

    actualización del la url

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-urban_routes_url = 'https://cnt-b8ad8f0b-8497-4e97-a3c3-d82196cf35f1.containerhub.tripleten-services.com?lng=es'
+urban_routes_url = 'https://cnt-e6464b11-3c1f-43da-9929-0e4451901135.containerhub.tripleten-services.com?lng=es'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
 phone_number = '+1 123 123 12 12'
